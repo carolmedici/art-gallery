70be159b30178139b5af8ffb1d54dd90cdea333f
carolmedici <carolina.obstetriz@gmail.com>
2023-04-01 22:38:53 -0300

    :test_tube: banner images

--- src/assets/Components/Slide/index.jsx
+++ src/assets/Components/Slide/index.jsx
@@ -1,29 +1,43 @@
 import React from 'react';
 import ImageGallery from 'react-image-gallery';
-import styles from './Slide.module.scss'
-import course from './course.png'
-import exhibition from './gaudi.png'
-import exhibition2 from './tarsila.png'
+import 'react-image-gallery/styles/css/image-gallery.css';
+import styles from './Slide.module.scss';
+import course from './course.png';
+import exhibition from './gaudi.png';
+import exhibition2 from './tarsila.png';
 
 
 export default class Slide extends React.Component {
-    render() {
-        const images = [
-            {
-                original: course,
-                
-            },
-            {
-                original: exhibition,
-            },
-            {
-                original: exhibition2,
-               
-            },
-                        
-        ]
-        return (
-            <ImageGallery items={images} autoPlay={true} />
-        );
-    }
+  render() {
+    const images = [
+      {
+        original: course,
+      },
+      {
+        original: exhibition,
+      },
+      {
+        original: exhibition2,
+      },
+    ];
+    return (
+      <div className={styles.slide}>
+        <ImageGallery
+          items={images}
+          autoPlay={true}
+          showFullscreenButton={false}
+          showPlayButton={false}
+          renderItem={(item) => (
+            <div className={styles.slide}>
+              <img
+                src={item.original}
+                alt={item.originalAlt}
+                style={{ width: '50%', height: 'auto' }}
+              />
+            </div>
+          )}
+        />
+      </div>
+    );
+  }
 }
--- src/assets/HomePage/index.jsx
+++ src/assets/HomePage/index.jsx
@@ -1,23 +1,24 @@
 import Header from '../Components/Header'
 import Menu from '../Components/Menu'
 import Slide from '../Components/Slide'
-import './HomePage.css'
-
+import styles from './HomePage.module.scss'
 
 const HomePage = () => {
-return(
-    <>
-        <Header />
-        <main>
-            <section>
-                <Menu />
-                <div>
-                    <Slide />
-                </div>
-            </section>
-        </main>
-    </>
-)
+    return(
+        <>
+            <Header />
+            <main>
+                <section className={styles.principal}>
+                    <div className={styles.principal__menu}>
+                        <Menu />
+                    </div>
+                    <div className={styles.principal__imagem}>
+                        <Slide />
+                    </div>
+                </section>
+            </main>
+        </>
+    )
 }
 
 export default HomePage
